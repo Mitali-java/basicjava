d0e2a3ef6d7b1ab238e9324bdedd631a7ad292a2
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-07-06 20:15:41 -0700

    Add files via upload

---
+++ Application.java
@@ -0,0 +1,19 @@
+
+public class Application {
+	public static void main(String[] args) {
+		System.out.println("Hello world");
+		
+		System.out.println("20+30=50");
+		System.out.println("20x30=600");
+		
+		int number = 15;
+		
+		int reminder;
+		reminder = number %2;
+		if(reminder == 0) {
+				System.out.println("Number is even.");
+		}else {
+			System.out.println("Number is odd.");
+	}
+	}
+	}
