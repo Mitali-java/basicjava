43429ad12e09003bdb43061581001d375069b986
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-07-21 21:54:58 -0700

    Add files via upload

---
+++ EvenOrOdd.java
@@ -0,0 +1,14 @@
+package EvenOrOdd;
+
+public class EvenOrOdd {
+
+	public static void main(String[] args) {
+
+		int x = 13;
+		if(13 % 2 == 0)
+	            System.out.println(x + " is even");
+	    else
+	            System.out.println(x + " is odd");
+	}
+	
+}
