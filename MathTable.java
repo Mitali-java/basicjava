48d78624296935fe74068ff75bb58f965bd52819
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-07-21 22:31:35 -0700

    Add files via upload

---
+++ MathTable.java
@@ -0,0 +1,12 @@
+package MathTable;
+
+public class MathTable {
+
+	public static void main(String[] args) {
+		int tableNumber = 6;
+		for (int x = 1; x <= 10; x++) {
+		 System.out.println(tableNumber + "*" + x + "=" + tableNumber*x);
+	
+		}
+	}
+}
