7c1dbb1672da67cff6b89611428090a7cf7e8717
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-08-04 21:08:12 -0700

    Add files via upload
    Updated with Array List

--- TohGameTower.java
+++ TohGameTower.java
@@ -1,12 +1,41 @@
 package Tower;
 
-public class Tower {
-	public String name;
+import java.util.*;
 
+public class TohGameTower {
+	private String name;
+	private ArrayList<Disc> discs;
+	
+	TohGameTower(String name) {
+		this.name = name;
+		discs = new ArrayList<Disc>();
+		
+	}
+
+	void addDisc(Disc d) {
+		discs.add(d);
+	}
+	
 	void draw() {
+		
+		//drawing the pole
 		drawPole();
+		
+		// delegate drawing to disc
+		for(Disc d : discs) {
+			d.draw();
+		}
+		
+		//base comes
 		drawBase();
+		
+		//title
 		drawTitle();
+	}
+	
+	Disc removeDisc(){
+		Disc topDisc = this.discs.remove(0);
+		return topDisc;
 	}
 
 	private void drawPole() {
@@ -16,7 +45,7 @@ public class Tower {
 	}
 
 	private void drawBase() {
-		System.out.println("    ----------");
+		System.out.println("   ------------");
 	}
 
 	private void drawTitle() {
