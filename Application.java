80666523fe75028c4ebcf4b0cc5799a118017675
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-08-11 21:11:20 -0700

    Add files via upload
    CRC Static project

--- Application.java
+++ Application.java
@@ -1,31 +1,24 @@
-package AddSpecialExample;
+package Learn;
 
 public class Application {
 
 	public static void main(String[] args) {
 
-		Math mathA = new Math(1);
-		Math mathB = new Math(2);
-		Math mathC = new Math(3);
-
-		int value = mathA.addSpecial(4);
-		System.out.println("From mathA: " + value); // supposed to come as 5 (1+4)
-
-		value = mathB.addSpecial(4);
-		System.out.println("From mathB: " + value); // supposed to come as 6 (2+4)
-
-		value = mathA.addSpecial(7); // supposed to come as 12 (5+7)
-		System.out.println("From mathA: " + value);
-
-		value = mathB.addSpecial(7); // supposed to come as 13 (6+7)
-		System.out.println("From mathB: " + value);
-
-		value = mathC.addSpecial(7); // supposed to come as 10 (3+7)
-		System.out.println("From mathC: " + value);
-
-		value = mathC.addSpecial(8); // supposed to come as 18 (10+8)
-		System.out.println("From mathC: " + value);
-
+	Student s1 = new Student();
+	s1.name = "Mitali";
+	Student.schoolName = "Sunset";
+	s1.readBook();
+	
+	Student s2 = new Student();
+	s2.name = "Sanika";
+	s2.readBook();
+	
+	Student s3 = new Student();
+	s3.name = "Chaudhari";
+	s3.readBook();
+	
+	Student.displaySchoolName();
+	
 	}
 
 }
--- Student.java
+++ Student.java
@@ -1,34 +1,22 @@
-package SchoolSystem;
+package Learn;
 
 public class Student {
+	
+	//instance variable
 	String name;
-	int age;
-	{
-		Student studentA;
-		Student studentB;
-
-		studentA = new Student();
-		studentA.name = "Mitali";
-		studentA.age = 14;
-
-		studentB = new Student();
-		studentB.name = "Sanika";
-		studentB.age = 15;
-
-		studentA.readBook();
-		studentB.readABook("The Lunar Chronicles");
-
+	
+	//class variable
+	static String schoolName;
+	
+	//instance method
+	public void readBook() {
+		System.out.println("Reading by: " + this.name);
+		System.out.println("My school name is: " + schoolName);
 	}
-
-	{
-
+	
+	//class method
+	public static void displaySchoolName() {
+		System.out.println("school name is: " + schoolName);
+		//System.out.println("name is:" + name);
 	}
-
-	void readBook() {
-	}
-
-	void readABook(String bookName) {
-		System.out.println(bookName);
-	}
-
 }
