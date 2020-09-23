4d02b2332f067bd07b962e2dfd25009154ec3e6f
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-09-22 22:41:27 -0700

    Add files via upload

---
+++ JavaTest/Q5MultiPackages.java
@@ -0,0 +1,21 @@
+package Package;
+
+public class Q5MultiPackages {
+
+	public static void main(String[] args) {
+		
+		Package1.Strawberry 
+		package1Strawberry = new Package1.Strawberry();
+
+		Package2.Strawberry
+		package2Strawberry = new Package2.Strawberry();
+		
+		
+		System.out.println(package1Strawberry.getSmoothie());
+		System.out.println(package2Strawberry.getSmoothie());
+
+	}
+
+}
+
+
