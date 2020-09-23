ab2fee27aa76ff65628c2e0211a0a303a764a56c
Mitali-java <67352686+Mitali-java@users.noreply.github.com>
2020-09-22 23:50:58 -0700

    Add files via upload

---
+++ JavaTest/Q9SchoolRegistration.java
@@ -0,0 +1,27 @@
+package Package;
+
+public class Q9SchoolRegistration {
+
+	public static void main(String[] args) {
+
+	}
+
+}
+
+class Department {
+
+	String name;
+
+	void getName() {
+
+	}
+}
+
+class Student {
+
+	int age;
+
+	void getAge() {
+
+	}
+}
